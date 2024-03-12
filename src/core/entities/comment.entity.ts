31a4c5c021b76d5a23878f2cc90d0c112696a5e4
Emmanuel Nwankwo <cnwankwom@gmail.com>
2024-03-12 16:02:34 +0100

    [feat] add comment module

---
+++ src/core/entities/comment.entity.ts
@@ -0,0 +1,23 @@
+import { Entity, Column, PrimaryGeneratedColumn } from 'typeorm'
+import { type Comment as IComment } from '../interface/comment.interface'
+
+@Entity({ name: 'comments' })
+export class Comment implements IComment {
+  @PrimaryGeneratedColumn('uuid')
+    id: string
+
+  @Column({ name: 'movie_id', type: 'int' })
+    movieId: number
+
+  @Column({
+    length: 500,
+    type: 'varchar'
+  })
+    content: string
+
+  @Column({ name: 'ip_address', type: 'varchar', length: 15 })
+    ipAddress: string
+
+  @Column({ type: 'timestamp', default: () => 'CURRENT_TIMESTAMP_UTC', name: 'created_date_in_utc' })
+    createdDate: Date
+}
