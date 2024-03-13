be5015201bf623bab0797957bf7e5544b5131105
Emmanuel Nwankwo <cnwankwom@gmail.com>
2024-03-13 07:32:20 +0100

    [feat] add pagination to get list

--- src/modules/comment/comment.service.ts
+++ src/modules/comment/comment.service.ts
@@ -3,23 +3,41 @@ import { Repository } from 'typeorm'
 import { InjectRepository } from '@nestjs/typeorm'
 import { Comment } from '../../core/entities/comment.entity'
 import { type Comment as IComment } from '../../core/interface/comment.interface'
+import { PageOptionsDto } from 'src/common/dtos/page-options.dto'
+import { PageDto } from 'src/common/dtos/page.dto'
+import { PageMetaDto } from 'src/common/dtos/page-meta.dto'
 
 @Injectable()
 export class CommentService {
-  constructor (@InjectRepository(Comment)
+  constructor(@InjectRepository(Comment)
   private readonly commentRepository: Repository<Comment>) { }
 
-  async create (comment: Comment): Promise<IComment> {
+  async create(comment: Comment): Promise<IComment> {
     return await this.commentRepository.save(comment)
   }
 
-  async findCommentsByMovieId (movieId: number): Promise<IComment[]> {
+  async findCommentsByMovieId(movieId: number): Promise<IComment[]> {
     return await this.commentRepository.find({ where: { movieId } })
   }
 
-  async getCommentCount (movieId: number): Promise<number> {
+  async getCommentCount(movieId: number): Promise<number> {
     return await this.commentRepository.findAndCount({ where: { movieId } }).then(([comments, count]) => {
       return count
     })
+  }
+
+  async getAllComments(pageOptionsDto: PageOptionsDto): Promise<PageDto<IComment>> {
+    const queryBuilder = this.commentRepository.createQueryBuilder('comment');
+    queryBuilder
+      .orderBy('created_date_in_utc', pageOptionsDto.order)
+      .skip(pageOptionsDto.skip)
+      .take(pageOptionsDto.take);
+
+    const itemCount = await queryBuilder.getCount();
+    const { entities } = await queryBuilder.getRawAndEntities();
+
+    const pageMetaDto = new PageMetaDto({ itemCount, pageOptionsDto });
+
+    return new PageDto(entities, pageMetaDto);
   }
 }
