84b2f46bc3ca51b090c5459b60e868ef7c628bbb
Emmanuel Nwankwo <cnwankwom@gmail.com>
2024-03-13 07:33:17 +0100

    [feat] add query class

--- src/modules/character/dto/query.dto.ts
+++ src/modules/character/dto/query.dto.ts
@@ -1,5 +1,41 @@
-export class QueryDto {
-  sort: string
-  direction: string
-  filterByGender: string
+import { ApiProperty, ApiPropertyOptional } from "@nestjs/swagger";
+import { IsEnum, IsOptional } from "class-validator";
+import { PageMetaDto } from "src/common/dtos/page-meta.dto";
+import { PageOptionsDto } from "src/common/dtos/page-options.dto";
+
+export enum SortBy {
+  NAME = 'name',
+  GENDER = 'gender',
+  HEIGHT = 'height',
+}
+
+export enum Gender {
+  MALE = 'male',
+  FEMALE = 'female'
+}
+
+export class QueryDto extends PageOptionsDto {
+  @ApiPropertyOptional({ enum: SortBy, default: SortBy.NAME })
+  @IsEnum(SortBy)
+  @IsOptional()
+  readonly sortBy: string
+
+  @ApiPropertyOptional({ enum: Gender })
+  @IsEnum(Gender)
+  @IsOptional()
+  readonly filterByGender?: string
+}
+
+export class MoviePageMetaDto extends PageMetaDto {
+  @ApiProperty()
+  readonly totalheightInCM: string;
+
+  @ApiProperty()
+  readonly totalheightInFT: string;
+
+  constructor({ itemCount, pageOptionsDto, totalheightInCM, totalheightInFT }: any) {
+    super({ itemCount, pageOptionsDto });
+    this.totalheightInCM = totalheightInCM;
+    this.totalheightInFT = totalheightInFT;
+  }
 }
